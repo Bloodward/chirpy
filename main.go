9af0105d88ae3b370349eec6ba65545e5727b674
bloodward <kevfry12@gmail.com>
2026-08-04 10:34:59 -0500

    added html file

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Printf("Hello, World!\n")
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Println("Serving on : 8080")
+	log.Fatal(srv.ListenAndServe())
 }
