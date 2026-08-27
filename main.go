133ca9151c65b0c79e89960bebe55b874c49a2b7
bloodward <kevfry12@gmail.com>
2026-08-27 17:55:09 -0500

    added admin changes to api

--- main.go
+++ main.go
@@ -19,8 +19,8 @@ func main() {
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc((http.StripPrefix("/app", http.FileServer(http.Dir("."))))))
 
-	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 
@@ -47,9 +47,14 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())))
+	w.Write([]byte(fmt.Sprintf(`<html>
+	<body>
+ 	 <h1>Welcome, Chirpy Admin</h1>
+	 <p>Chirpy has been visited %d times!</p>
+	</body>
+	<html>`, cfg.fileserverHits.Load)))
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
